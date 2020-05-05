4a89fed6cf926cd95b22b260f83265872d6539e6
mattalagala <matt15y@gmail.com>
2020-05-05 17:34:39 -0500

    Answer : How Many Confections Supplier?

---
+++ viewfindbeveragesuppliers_confections.sql
@@ -0,0 +1,17 @@
+INSERT INTO public.viewfindbeveragesuppliers (category_id,category_name,description,company_name) VALUES 
+(3,'Confections','Desserts, candies, and sweet breads','Pavlova, Ltd.')
+,(3,'Confections','Desserts, candies, and sweet breads','Specialty Biscuits, Ltd.')
+,(3,'Confections','Desserts, candies, and sweet breads','Specialty Biscuits, Ltd.')
+,(3,'Confections','Desserts, candies, and sweet breads','Specialty Biscuits, Ltd.')
+,(3,'Confections','Desserts, candies, and sweet breads','Heli SÃ¼ÃŸwaren GmbH & Co. KG')
+,(3,'Confections','Desserts, candies, and sweet breads','Heli SÃ¼ÃŸwaren GmbH & Co. KG')
+,(3,'Confections','Desserts, candies, and sweet breads','Heli SÃ¼ÃŸwaren GmbH & Co. KG')
+,(3,'Confections','Desserts, candies, and sweet breads','Zaanse Snoepfabriek')
+,(3,'Confections','Desserts, candies, and sweet breads','Zaanse Snoepfabriek')
+,(3,'Confections','Desserts, candies, and sweet breads','Karkki Oy')
+;
+INSERT INTO public.viewfindbeveragesuppliers (category_id,category_name,description,company_name) VALUES 
+(3,'Confections','Desserts, candies, and sweet breads','Karkki Oy')
+,(3,'Confections','Desserts, candies, and sweet breads','ForÃªts d''Ã©rables')
+,(3,'Confections','Desserts, candies, and sweet breads','Specialty Biscuits, Ltd.')
+;
